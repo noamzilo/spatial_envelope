391fe47a57fa1f760cfbec3a710b0270925e6731
Noam <noams@epd-medical.com>
2019-12-04 00:23:12 +0200

    calculating for train and test sets

--- features/Sift.py
+++ features/Sift.py
@@ -3,24 +3,32 @@ import cv2
 
 
 class SiftDetector(object):
-    def __init__(self, data_set, num_features_per_image = 200):
+    def __init__(self, data_set):
         self._data_set = data_set
         self._x_train, self._x_test, self._y_train, self._y_test =\
             self._data_set.x_train, self._data_set.x_test, self._data_set.y_train, self._data_set.y_test
         self._n_train = len(self._y_train)
+        self._n_test = len(self._y_test)
         self._detector = cv2.xfeatures2d.SIFT_create()
+        self.train_features = None
+        self.test_features = None
 
-    def calculate_features(self):
-        # retval = cv.xfeatures2d.SIFT_create([,nfeatures[,nOctaveLayers[,contrastThreshold[,edgeThreshold[,sigma]]]]])
-        train_features = []
+    def calculate_features_train(self):
+        self.train_features = []
         for i in range(self._n_train):
-            features = self._detector.detect(self._data_set.x_train[i, :, :], None)
-            train_features.append(features)
-        hi=5
+            features = self._detector.detect(self._x_train[i, :, :], None)
+            self.train_features.append(features)
+
+    def calculate_features_test(self):
+        self.test_features = []
+        for i in range(self._n_test):
+            features = self._detector.detect(self._x_test[i, :, :], None)
+            self.test_features.append(features)
 
 if __name__== "__main__":
     def main():
         data_set = load_default()
         sift_detector = SiftDetector(data_set)
-        sift_detector.calculate_features()
+        sift_detector.calculate_features_train()
+        sift_detector.calculate_features_test()
     main()
