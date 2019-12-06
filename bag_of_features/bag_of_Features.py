e99571edbabc5ec5e945b38533259c25abc67688
Noam <noams@epd-medical.com>
2019-12-06 21:56:47 +0200

    better features and descriptors separation

--- bag_of_features/bag_of_Features.py
+++ bag_of_features/bag_of_Features.py
@@ -3,15 +3,23 @@ from features.Sift import SiftDetector
 
 
 class BagOfFratures(object):
-    def __init__(self, featurs):
-        self._featues = featurs
+    def __init__(self, keppoints_per_image, descriptors_per_image):
+        self._keppoints_per_image = keppoints_per_image
+        self._descriptors_per_image = descriptors_per_image
+
+        hi=5
+
+    def calculate_k_means(self, k=100):
+        print(k)
 
 
 if __name__ == "__main__":
     def main():
         data_set = load_default()
         sift_detector = SiftDetector(data_set)
-        bof = BagOfFratures(sift_detector)
+        sift_detector.calculate_features_train()
+        bof = BagOfFratures(sift_detector.train_keypoints, sift_detector.train_descriptors)
+        bof.calculate_k_means(k=100)
         
 
     main()
--- features/Sift.py
+++ features/Sift.py
@@ -5,26 +5,31 @@ from sklearn.cluster import KMeans
 
 class SiftDetector(object):
     def __init__(self, data_set):
+        # https://docs.opencv.org/master/da/df5/tutorial_py_sift_intro.html
         self._data_set = data_set
         self._x_train, self._x_test, self._y_train, self._y_test =\
             self._data_set.x_train, self._data_set.x_test, self._data_set.y_train, self._data_set.y_test
         self._n_train = len(self._y_train)
         self._n_test = len(self._y_test)
         self._detector = cv2.xfeatures2d.SIFT_create()
-        self.train_features = None
-        self.test_features = None
+        self._train_features = None
+        self._test_features = None
+        self.train_keypoints, self.train_descriptors = None, None
+        self.test_keypoints, self.test_descriptors = None, None
 
     def calculate_features_train(self):
-        self.train_features = []
+        self._train_features = []
         for i in range(self._n_train):
-            features = self._detector.detectAndCompute(self._x_train[i, :, :], None)
-            self.train_features.append(features)
+            keypoints, descriptors = self._detector.detectAndCompute(self._x_train[i, :, :], None)
+            self._train_features.append((keypoints, descriptors))
+        self.train_keypoints, self.train_descriptors = list(zip(*self._train_features))
 
     def calculate_features_test(self):
-        self.test_features = []
+        self._test_features = []
         for i in range(self._n_test):
-            features = self._detector.detectAndCompute(self._x_test[i, :, :], None)
-            self.test_features.append(features)
+            keypoints, descriptors = self._detector.detectAndCompute(self._x_test[i, :, :], None)
+            self._test_features.append((keypoints, descriptors))
+        self.test_keypoints, self.test_descriptors = list(zip(*self._test_features))
 
 if __name__== "__main__":
     def main():
