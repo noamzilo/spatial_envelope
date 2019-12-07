67d49ade55710699896bc1ec3efa1fabbdbf60bb
Noam <noams@epd-medical.com>
2019-12-07 17:02:44 +0200

    made svm maybe show an roc and auc

--- svm/svm.py
+++ svm/svm.py
@@ -19,45 +19,52 @@ class Svm(object):
         predicions = self._classifier.predict(features)
         return predicions
 
-    def show_roc(self, test_bag_of_features, test_labels):
-        metrics.plot_roc_curve(self._classifier, test_bag_of_features, test_labels)
-    
+    # def show_roc(self, test_bag_of_features, test_labels):
+    #     metrics.plot_roc_curve(self._classifier, test_bag_of_features, test_labels)
+    #
+
 
 def calculate_svm_roc(train_bag_of_features, test_bag_of_features, train_labels, test_labels):
     num_c_tries = 10
     available_cs = np.logspace(start=-2, stop=5.0, num=num_c_tries, base=10.0)
     true_positives = np.zeros(num_c_tries)
     false_positives = np.zeros(num_c_tries)
-    # for i, c in enumerate(available_cs):
-    #     svm = Svm(c=1)
-    #     svm.train(train_bag_of_features, train_labels)
-    #     predicions = svm.predict(test_bag_of_features)
-    #     confusion = confusion_matrix(test_labels, predicions)
-    #     tn, fn, tp, fp = confusion[0, 0], confusion[1, 0], confusion[0, 1], confusion[1, 1]  # true/false positive/negative
-    #
-    #     true_positives[i] = tp
-    #     false_positives[i] = fp
-    #
-    # return np.array(true_positives)/len(test_labels), np.array(false_positives)/len(test_labels)
+    for i, c in enumerate(available_cs):
+        svm = Svm(c=1)
+        svm.train(train_bag_of_features, train_labels)
+        predicions = svm.predict(test_bag_of_features)
+        confusion = confusion_matrix(test_labels, predicions)
+        tn, fn, tp, fp = confusion[0, 0], confusion[1, 0], confusion[0, 1], confusion[1, 1]  # true/false positive/negative
 
-    svm = Svm(c=1)
-    svm.train(train_bag_of_features, train_labels)
-    # predicions = svm.predict(test_bag_of_features)
-    svm.show_roc(test_bag_of_features, test_labels)
+        true_positives[i] = tp
+        false_positives[i] = fp
+
+    return np.array(true_positives)/len(test_labels), np.array(false_positives)/len(test_labels)
+
+    # svm = Svm(c=1)
+    # svm.train(train_bag_of_features, train_labels)
+    # # predicions = svm.predict(test_bag_of_features)
+    # svm.show_roc(test_bag_of_features, test_labels)
 
 
-# def show_roc(true_positives, false_positives):
-#     assert true_positives.shape[0] == false_positives.shape[0]
-#     roc_auc = auc(fpr, tpr)
-#     plt.figure()
-#     plt.title("Roc curve")
-#     plt.show()
+def show_roc(true_positives, false_positives):
+    assert true_positives.shape[0] == false_positives.shape[0]
+    roc_auc = auc(false_positives, true_positives)
+    plt.figure()
+    plt.title("Roc curve")
+    plt.xlabel('False positive ratio')
+    plt.ylabel('True positive ratio')
+    plt.plot(false_positives, true_positives)
+    plt.show(block=True)
+    print(f"roc auc: {roc_auc}")
+
 
 if __name__ == "__main__":
     def main():
         train_bag_of_features, test_bag_of_features, train_labels, test_labels \
             = calculate_bag_of_features_for_default_dataset()
 
-        calculate_svm_roc(train_bag_of_features, test_bag_of_features, train_labels, test_labels)
+        tpr, fpr = calculate_svm_roc(train_bag_of_features, test_bag_of_features, train_labels, test_labels)
+        show_roc(tpr, fpr)
 
     main()
